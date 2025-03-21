414d4a7ce0c86ee30cf95cd57352fbdff928209e
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2025-03-21 16:31:19 +0300

    fetch products

---
+++ src/components/ProductList.js
@@ -0,0 +1,49 @@
+import { useCallback, useEffect, useState } from "react";
+import { useFetch } from "../hooks/useFetch";
+
+export const ProductList = () => {
+  //   const [products, setProducts] = useState([]);
+  const [url, setUrl] = useState("http://localhost:8000/products");
+
+  //   const fetchProducts = useCallback(async () => {
+  //     try {
+  //       const response = await fetch(url);
+  //       const data = await response.json();
+  //       setProducts(data);
+  //     } catch (err) {
+  //       setProducts([]);
+  //     }
+  //   }, [url]);
+
+  //   useEffect(() => {
+  //     fetchProducts();
+  //   }, [fetchProducts]);
+
+  const { data: products } = useFetch(url);
+
+  return (
+    <section>
+      <button onClick={() => setUrl("http://localhost:8000/products")}>
+        All
+      </button>
+      <button
+        onClick={() => setUrl("http://localhost:8000/products?in_stock=true")}
+      >
+        In stock only
+      </button>
+      {products &&
+        products.map((product) => {
+          return (
+            <div className="card" key={product.id}>
+              <p>{product.id}</p>
+              <p>{product.name}</p>
+              <p>
+                <span>$ {product.price}</span>
+                <span>{product.in_stock ? "In stock" : "Out of stock"}</span>
+              </p>
+            </div>
+          );
+        })}
+    </section>
+  );
+};
