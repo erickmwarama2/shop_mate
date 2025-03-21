50c2e7df2516619c379a78b5d8833644574484b9
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2025-03-21 20:29:59 +0300

    add catch all route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Home } from "./components/Home";
+import { PageNotFound } from "./components/PageNotFound";
 import { ProductDetail } from "./components/ProductDetail";
 import { ProductList } from "./components/ProductList";
 import { Routes, Route, Navigate } from "react-router-dom";
@@ -18,6 +19,7 @@ function App() {
           <Route path="/products/:id" element={<ProductDetail />}></Route>
           <Route path="/contact" element={<Contact />} />
           <Route path="/admin" element={<Navigate to="/products/10001" />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </main>
       <Footer />
